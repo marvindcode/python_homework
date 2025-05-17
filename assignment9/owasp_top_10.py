1f122979c742e74f89a5cd539b0cd6ddb144cdbe
Marvin Diaz <md1685268@gmail.com>
2025-05-17 13:00:28 -0700

    Updated files
    :wq

--- assignment9/owasp_top_10.py
+++ assignment9/owasp_top_10.py
@@ -8,12 +8,13 @@ import pandas as pd
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("https://owasp.org/www-project-top-ten/")
 
-vulnerabilities_10 = driver.find_elements(By.XPATH, '//a[starts-with(@href, "/Top10/A0")]')
+vulns_10 = driver.find_elements(By.XPATH, '//a[starts-with(@href, "/Top10/A0")]')
+
 
 results = []
-for vuln in vulnerabilities_10:
-    title = vuln.text.strip()
-    href = vuln.get_attribute('href')
+for v in vulns_10:
+    title = v.text.strip()
+    href = v.get_attribute("href")
     if title and href:
         results.append({"Title": title, "Link": href})
 
@@ -21,6 +22,7 @@ driver.quit()
 
 df = pd.DataFrame(results)
 df.to_csv("owasp_top_10.csv", index=False)
+
 
 print(df.head(10))
 
